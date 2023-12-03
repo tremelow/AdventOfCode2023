4cab3b479b11825ce85bcba538acc17afd0c63b9
Léopold Trémant <29092877+tremelow@users.noreply.github.com>
2023-12-03 14:15:45 +0100

    Day 3 - fixed swap between src and test

--- src/AdventOfCode23.jl
+++ src/AdventOfCode23.jl
@@ -6,5 +6,7 @@ include("day1.jl")
 export day1a, day1b
 include("day2.jl")
 export day2a, day2b
+include("day3.jl")
+export day3a, day3b
 
 end # module AdventOfCode23
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,7 @@ end
 @testset "Day 2" begin
     include("day2.jl")
 end
+@testset "Day 3" begin
+    include("day3.jl")
+end
 
